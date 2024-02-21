f685c021cdf774fe9d9580c4176001f53d780521
SHIN SUJUNG <SHIN SUJUNG@DESKTOP-HC89P6A>
2024-02-21 17:47:05 +0900

    feat : MemberInfo / Servlet -> Spring Controller 전환

--- src/main/java/com/service/PostService.java
+++ src/main/java/com/service/PostService.java
@@ -3,6 +3,7 @@ package com.service;
 import java.util.HashMap;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,13 +15,11 @@ import com.dto.board.PostDTO;
 import com.dto.board.PostPageDTO;
 import com.dto.board.PostSaveDTO;
 
-import lombok.RequiredArgsConstructor;
-
 @Service
-@RequiredArgsConstructor
 @Transactional
 public class PostService {
 
+	@Autowired
 	PostDAO dao;
 
 	// 글 추가
